384d0932aa0f7ea040d9ef5965bcea45b9c9016d
Rahul <agarwalr327@gmail.com>
2023-11-13 18:45:30 +0530

    working on main page of the portfolio

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
+
+export default function Home({ repositories }) {
+  return (
+    <ContainerBlock>
+      <Hero />
+      {/* <FavouriteProjects /> */}
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  );
+}
+
+export const getServerSideProps = async () => {
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
