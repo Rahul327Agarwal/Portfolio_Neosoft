a8cff242aed8540928adcd54c488e2ca85f73bb9
Rahul <agarwalr327@gmail.com>
2023-11-16 16:15:17 +0530

    img tage changed to Image

--- components/MainPageMidSection.js
+++ components/MainPageMidSection.js
@@ -40,6 +40,8 @@ export default function MainPageMidSection() {
         <div className="w-3/4 ">
           <Image
             src={userData.avatarUrl}
+            width={400}
+            height={400}
             alt="myProfilePic"
             className={Styles.profilePic}
           />
